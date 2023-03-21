ad314a01f5987b78f7a6a37defe855e39241fb99
Benjamin GRULOIS <bgrulois@e1r4p12.clusters.42paris.fr>
2023-03-21 07:54:29 +0100

    raycasting niveau cp

--- srcs/render_3d.c
+++ srcs/render_3d.c
@@ -6,7 +6,7 @@
 /*   By: bgrulois <bgrulois@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 15:24:36 by bgrulois          #+#    #+#             */
-/*   Updated: 2023/03/21 06:50:14 by bgrulois         ###   ########.fr       */
+/*   Updated: 2023/03/21 07:54:00 by bgrulois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	draw_slice(t_game *g, t_slice *slice)
 {
 	int	i;
-	int	y_max = 720;
+	int	y_max = 1250;
 
 	i = 0;
 	while (i <= y_max)
@@ -30,7 +30,8 @@ void	draw_slice(t_game *g, t_slice *slice)
 	}
 }
 
-void	fill_image(t_game *g)
+/*
+void	fill_image(t_game *g)				//debug funct
 {
 	int	x_max = 1080;
 	int	y_max = 720;
@@ -48,29 +49,26 @@ void	fill_image(t_game *g)
 	}
 	return ;
 }
+*/
 
 void	render_image(t_game *g)
 {
-	int	x_max = 1080;
-	int	y_max = 720;
-	int	i = -1;
-	//int	wall_size;
+	int	x_max = 1920;				//screen res width
+	int	y_max = 1250;				//screen res height
+	int	i = 0;
+	int	wall_size;
 	t_pos	*p;
 	t_slice	slice;
 
 
 	g->img->img = mlx_new_image(g->mlx, 2000, 1500);
 	g->img->addr = mlx_get_data_addr(g->img->img, &g->img->bits_per_pixel, &g->img->line_length, &g->img->endian);
-	//fill_image(g);
-	//mlx_put_image_to_window(g->mlx, g->win, g->img->img, 0, 0);
 	p = insert_rays(g);
-	//wall_size = 1 / p->info->dist * y_max;
-	slice.pos_x = 0;//p->info->pos_x;
-	//slice.wall_start = wall_size / 2 - (int)p->info->dist / 2;
-	//slice.wall_end = wall_size / 2 + (int)p->info->dist / 2;
-	//slice.wall_start = y_max / 2 - (int)p->info->dist / 2;
-	//slice.wall_end = y_max / 2 + (int)p->info->dist / 2;
-	//int	lol = 0;
+	if (p->info->dist <= 0)
+		p->info->dist = 1;
+	wall_size = (y_max * 2) / (int)p->info->dist;
+	slice.pos_x = 0;
+	//int	lol = 0;				//debug
 	/*while (p->info->next)
 	{
 		printf("ray ? %i\n\n", p->info->dist);
@@ -78,31 +76,19 @@ void	render_image(t_game *g)
 		lol++;
 	}*/
 	//printf("lol ---> %i\n\n\n", lol);
-	printf("ray_dist == %i\n\n\n\n\n\n", (int)p->info->dist);
-	while (++i <= x_max)
+	while (i <= x_max)
 	{
-		//draw_slice(g, &slice);
-		//i++;
-		slice.pos_x = i;//p->info->pos_x;
+		slice.pos_x = i;
 		draw_slice(g, &slice);
-		i++;
-		//slice.pos_x = i;//p->info->pos_x;
-		//draw_slice(g, &slice);
-		//i++;
-		//slice.pos_x = i;//p->info->pos_x;
-		//draw_slice(g, &slice);
 		if (!p->info->next)
 			return ;
 		p->info = p->info->next;
-		printf("ray_dist == %i\n\n\n\n\n\n", (int)p->info->dist);
 		if (p->info->dist <= 0)
 			p->info->dist = 1;
-		//wall_size = y_max / p->info->dist * 2;
-		slice.pos_x = i;//p->info->pos_x;
-		//slice.wall_start = y_max / 2 - wall_size / 2;
-		//slice.wall_end = y_max / 2 + wall_size / 2;
-		slice.wall_start = y_max / 2 - ((y_max * 2) / (int)p->info->dist);
-		slice.wall_end = y_max / 2 + ((y_max * 2) / (int)p->info->dist);
+		wall_size = (y_max * 4) / (int)p->info->dist;
+		slice.wall_start = y_max / 2 - wall_size;
+		slice.wall_end = y_max / 2 + wall_size;
+		i+=4;
 	}
 	mlx_put_image_to_window(g->mlx, g->win, g->img->img, 0, 0);
 }
