eaf549bad51eed545a5bde30acb57be7402a26b3
Benjamin GRULOIS <bgrulois@e1r13p14.clusters.42paris.fr>
2023-03-30 04:19:46 +0200

    norm update

--- srcs/render_3d.c
+++ srcs/render_3d.c
@@ -6,23 +6,31 @@
 /*   By: bgrulois <bgrulois@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 15:24:36 by bgrulois          #+#    #+#             */
-/*   Updated: 2023/03/30 03:28:57 by bgrulois         ###   ########.fr       */
+/*   Updated: 2023/03/30 04:19:20 by bgrulois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
+int	eval_t_y(int y, int ws, float ratio)
+{
+	float	t_y;
+
+	t_y = (float)(y - ws) / ratio;
+	return ((int)t_y);
+}
+
 void	draw_slice(t_game *g, t_slice *slice)
 {
 	int	y;
-	float	ratio = 0.0;
+	float	ratio;
 
 	y = 0;
 	ratio = (slice->wall_end - slice->wall_start) / TILE_SIZE_F;
 	if (ratio == 0.0)
-		ratio = 0.1;
+		ratio = 0.01;
 	if (ratio < 1.0 && ratio > 0.0)
-		ratio = 1 / ratio;
+		ratio = 1.0 / ratio;
 	//printf("\n$$$$$$$$$$$$$$$$$$\nTXT_XPOS(px) = %f\nRATIO = %f\n$$$$$$$$$$$$$$$$$$\n", slice->wall_pos, ratio);
 	while (y < RES_Y)
 	{
@@ -30,7 +38,9 @@ void	draw_slice(t_game *g, t_slice *slice)
 			my_mlx_pixel_put(g->img, slice->pos_x, y, g->t->fc[1]);
 		else if (y > slice->wall_start && y <= slice->wall_end && slice->wall_pos != 0)
 		{
-			g->img->addr[y * 4 * g->img->line_length / 4 + slice->pos_x * 4] = g->t->no_t.addr[(int)((float)(y - slice->wall_start) / ratio) * g->t->no_t.line_length + ((int)slice->wall_pos * 4)];
+			g->img->addr[y * 4 * g->img->line_length / 4 + slice->pos_x * 4] =
+				g->t->no_t.addr[eval_t_y(y, slice->wall_start, ratio)
+				* g->t->no_t.line_length + (int)(slice->wall_pos * 4)];
 		}
 		else if (y > slice->wall_end)
 			my_mlx_pixel_put(g->img, slice->pos_x, y, g->t->fc[0]);
@@ -38,29 +48,21 @@ void	draw_slice(t_game *g, t_slice *slice)
 	}
 }
 
-/*
-void	fill_image(t_game *g)				//debug funct
+void	init_slice(t_slice *slice, t_pos *p, int i)
 {
-	int	i = 0;
-	int	j = 0;
+	int	wall_size;
 
-	while (i < RES_X)
-	{
-		while (j < RES_Y)
-		{
-			my_mlx_pixel_put(g->img, j, i, g->t->fc[0]);
-			j++;
-		}
-		i++;
-	}
-	return ;
+	wall_size = ((RES_Y * TILE_SIZE) / (int)p->info->dist);
+	slice->pos_x = i;
+	slice->wall_pos = ((p->info->pos_x + p->info->pos_y) % TILE_SIZE) * 100.0 / TILE_SIZE_F;
+	slice->wall_start = RES_Y / 2 - wall_size / 2;
+	slice->wall_end = RES_Y / 2 + wall_size / 2;
+	slice->wall_pos = slice->wall_pos * TILE_SIZE_F / 100.0;
 }
-*/
 
 void	render_image(t_game *g)
 {
 	int	i;
-	int	wall_size;
 	t_pos	*p;
 	t_slice	slice;
 
@@ -70,27 +72,15 @@ void	render_image(t_game *g)
 	p = insert_rays(g);
 	if (p->info->dist <= 0)
 		p->info->dist = 1;
-	wall_size = ((RES_Y * TILE_SIZE) / (int)p->info->dist);
-	slice.pos_x = 0;
+	init_slice(&slice, p, i);
 	while (i < RES_X - 1)
 	{
-		slice.pos_x = i;
-		//printf("--------------------\nx = %i\n---------------------", slice.pos_x);
 		if (!p->info->next)
 			return ;
 		p->info = p->info->next;
 		if (p->info->dist <= 0)
 			p->info->dist = 1;
-		wall_size = ((RES_Y * TILE_SIZE) / (int)p->info->dist);
-		////////////////////////wip/////////////////////////////
-		slice.wall_pos = ((p->info->pos_x + p->info->pos_y) % TILE_SIZE) * 100.0 / TILE_SIZE_F;
-		printf("\n############wall_pos = %i\n###############\n", (int)slice.wall_pos);
-		//else
-		//	slice.wall_pos = (p->info->pos_y % TILE_SIZE) * 100 / TILE_SIZE;
-		////////////////////////////////////////////////////////
-		slice.wall_start = RES_Y / 2 - wall_size / 2;
-		slice.wall_end = RES_Y / 2 + wall_size / 2;
-		slice.wall_pos = slice.wall_pos * TILE_SIZE_F / 100.0;
+		init_slice(&slice, p, i);
 		draw_slice(g, &slice);
 		i++;
 	}
